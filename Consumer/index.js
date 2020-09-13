01a58fbe548459d38d316f55050dbcffc4818872
gustavo lima <gustavolima@pop-os.localdomain>
2020-09-13 14:55:42 -0300

    ajustes finais no projeto

--- Consumer/index.js
+++ Consumer/index.js
@@ -5,17 +5,51 @@ const path = require('path');
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
+// importa o Axios
+const axios = require("axios");
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
+// Configuar Sessão e o Flash
+const session = require('express-session');
+const flash = require('connect-flash');
+
+app.use(session({
+	secret:'sdasdafa58641a24sd21as0anidbaBNUIANDsksmdlskNDjkGUIQQMODSK<L',
+	saveUninitialized: true,
+	resave: true
+}));
+
+app.use(flash());
+
 // Rotas
 app.get("/", (req, res) =>{
     res.statusCode = 200 ;
-    res.render('main', {msg: "ola gustavo"});
+    res.render('login');
 });
+
+app.post('/home', (req, res) => {
+    var {email, password} = req.body; 	
+    
+    axios.post("http://localhost:8000/auth",{
+        email,
+        password
+    }).then(axiosResponse =>{
+        
+        if(axiosResponse.status == '200'){
+            res.render('main',{token: axiosResponse.data.token});
+        }else{
+            res.send("Erro de login");
+        }
+    }).catch(err => {
+        //req.flash('info', 'Falha de login!');
+        res.redirect("/");
+    });
+});
+
+
 
 
 app.listen(8050, () =>{
--- Producer/index.js
+++ Producer/index.js
@@ -1,10 +1,47 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+// API REST COM EXPRESS E JWT
 
 // configura o express
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+// Configura o CORS para permitir o acesso do Consumer a API
+const cors = require('cors');
+app.use(cors());
+
+// Configura JWT (chave de acesso)
+const jwt = require("jsonwebtoken");
+const JWTSecret = "16a5s11af5fasF54A16SD1A65W161d56asd1wd53d0HJMU1YJ";
+
+// Middleware
+function auth(req,res,next){
+    const authToken = req.headers['authorization'];
+    
+    if(authToken != undefined){
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+        jwt.verify(token, JWTSecret, (err, data)=>{
+            if(err){
+                res.status(401); //não autorizado
+                res.json({err: "Token inválido!"});
+            }else{
+                req.token = token;
+                req.loggedUser = {id: data.id, email: data.email};
+        
+                next();
+            }
+        });
+    }else{
+        res.status(401); //não autorizado
+        res.json({err: "Token inválido!"});
+    }
+    
+}
+
+
+
 
 
 // Banco de dados falso
@@ -28,11 +65,56 @@ var DB = {
             year: 2013,
             price: 100
         }
+    ],
+    users: [
+        {
+            id: 1,
+            name: "admin",
+            email: "admin",
+            password: "123456"
+        },
+        {
+            id: 2,
+            name: "convidado",
+            email: "convidado@email.com",
+            password: "111111"
+        }
     ]
 }
 
 // Rotas
-app.get("/games", (req, res) =>{
+app.post('/auth', (req, res) => {
+    var {email, password} = req.body;
+    if(email != undefined){
+        var user = DB.users.find(u => u.email == email);
+        if(user != undefined){
+            if(user.password == password){
+                // informações que serão associadas ao token, tempo que o token expira, etc
+                jwt.sign({id: user.id, email: user.email}, JWTSecret, {expiresIn:'48h'},(err,token) => {
+                    if(err){
+                        res.status(400);
+                        res.json({err:"Falha interna"});
+                    }else{
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                });
+            }else{
+                res.status(401);
+                res.json({err: "Credenciais inválidas!"});
+            }
+        }else{
+            res.status(404);
+            res.json({err: "O email enviado não existe na base de dados!"});
+        }
+    }else{
+        res.status(400);
+        res.json({err: "O email enviado é inválido!"});
+    }
+
+});
+
+app.get("/games", auth,(req, res) =>{
     res.statusCode = 200 ;
     res.json(DB.games);
 });
@@ -134,7 +216,9 @@ app.put('/game/:id', (req, res) => {
     
 });
 
-app.listen(8000, () =>{
-    console.log("API em funcionamento..");
+// Inicializa o servidor
+var porta = 8000;
+app.listen(porta, () =>{
+    console.log("API REST em funcionamento na porta " + porta);
 });
 
